71b758d138af2fab0e0775c7cacf070511ad97d0
Nick Fraser <nicholas.fraser@sydney.edu.au>
2015-07-15 15:16:52 +1000

    Added a new remez test. It findzero gets stick in a while loop with this test.

---
+++ test/remez_tests.m
@@ -0,0 +1,16 @@
+% Test the exp function.
+fun = inline('exp(x)');
+fun_der = inline('exp(x)');
+interval = [-8, 0];
+order = 5;
+
+A = remez(fun, fun_der, interval, order);
+
+A1 = A(1:end-1);  % the 3 coefficients of the second order polynomial
+E = A(end); % the maximum approximation error 
+
+numTestPoints = 100;
+x = linspace(interval(1), interval(2), numTestPoints);
+fx = feval(fun, x);
+fest = polyval(A1(end:-1:1), x-interval(1));
+ferr = err(x, fun, A1, interval(1));
